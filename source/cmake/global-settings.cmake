74e7137a8886cb67fe7e3e9870f1fe699e8d5226
tsvari <tsvari@hotmail.com>
2025-04-27 21:17:38 -0400

    Minor changes in global settings
    Update c++ 20 to c++ 23

--- source/cmake/global-settings.cmake
+++ source/cmake/global-settings.cmake
@@ -14,13 +14,15 @@ else()
     set(ALL_PROJECT_PATH "")
 endif()
 
+# All project definations
 set(ALL_PROJECT_PATH ${ALL_PROJECT_PATH})
 set(ALL_PROJECT_APPDATA_PATH ${ALL_PROJECT_PATH}/assets/app-data/)
-set(ALL_PROJECT_TEST_PATH ${ALL_PROJECT_PATH}/source/include/tests/app-data/)
+set(ALL_PROJECT_TEST_APPDATA_PATH ${ALL_PROJECT_PATH}/source/include/tests/app-data/)
 
 add_definitions("-DALL_PROJECT_PATH=\"${ALL_PROJECT_PATH}\"")
 add_definitions("-DALL_PROJECT_APPDATA_PATH=\"${ALL_PROJECT_APPDATA_PATH}\"")
-add_definitions("-DALL_PROJECT_TEST_PATH=\"${ALL_PROJECT_TEST_PATH}\"")
+add_definitions("-DALL_PROJECT_TEST_APPDATA_PATH=\"${ALL_PROJECT_TEST_APPDATA_PATH}\"")
+
 
 set(TEST_PROJECT "AllTestProject")
 add_definitions("-DTEST_PROJECT=\"${TEST_PROJECT}\"")
@@ -33,7 +35,10 @@ include_directories(${THIRD_PARTY_INCLUDE_DIR})
 include_directories(${THIRD_PARTY_INCLUDE_DIR}/markup)
 include_directories(${THIRD_PARTY_INCLUDE_DIR}/json-develop/include)
 
-# Backend settings
+# Backend projects definations and settings
+set(BACKEND_TEST_PROJECT "BackendTestProject")
+add_definitions("-DBACKEND_TEST_PROJECT=\"${BACKEND_TEST_PROJECT}\"")
+
 set(BACKEND_INCLUDE_DIR ${ALL_PROJECT_PATH}/source/backend/include)
 set(THIRD_PARTY_BACKEND_DIR ${BACKEND_INCLUDE_DIR}/3party)
 
@@ -41,7 +46,10 @@ include_directories(${BACKEND_INCLUDE_DIR})
 include_directories(${THIRD_PARTY_BACKEND_DIR})
 include_directories(${THIRD_PARTY_BACKEND_DIR}/easylogging)
 
-set(BACKEND_TEST_PROJECT "BackendTestProject")
-add_definitions("-DBACKEND_TEST_PROJECT=\"${BACKEND_TEST_PROJECT}\"")
+set(ALL_BACKEND_PROJECT_PATH ${ALL_PROJECT_PATH}/source/backend/)
+set(ALL_BACKEND_TEST_APPDATA_PATH ${BACKEND_INCLUDE_DIR}/tests/app-data/)
+
+add_definitions("-DALL_BACKEND_PROJECT_PATH=\"${ALL_BACKEND_PROJECT_PATH}\"")
+add_definitions("-DALL_BACKEND_TEST_APPDATA_PATH=\"${ALL_BACKEND_TEST_APPDATA_PATH}\"")
 
 
