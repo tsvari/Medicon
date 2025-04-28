4ea7144cb6f8aeebee21f81654a78febad00777e
tsvari <tsvari@hotmail.com>
2025-04-28 10:43:29 -0400

    Refactor cmake files to adopt folders' names changes

--- source/cmake/global-settings.cmake
+++ source/cmake/global-settings.cmake
@@ -17,7 +17,7 @@ endif()
 # All project definations
 set(ALL_PROJECT_PATH ${ALL_PROJECT_PATH})
 set(ALL_PROJECT_APPDATA_PATH ${ALL_PROJECT_PATH}/assets/app-data/)
-set(ALL_PROJECT_TEST_APPDATA_PATH ${ALL_PROJECT_PATH}/source/include/tests/app-data/)
+set(ALL_PROJECT_TEST_APPDATA_PATH ${ALL_PROJECT_PATH}/source/source/tests/app-data/)
 
 add_definitions("-DALL_PROJECT_PATH=\"${ALL_PROJECT_PATH}\"")
 add_definitions("-DALL_PROJECT_APPDATA_PATH=\"${ALL_PROJECT_APPDATA_PATH}\"")
@@ -27,7 +27,7 @@ add_definitions("-DALL_PROJECT_TEST_APPDATA_PATH=\"${ALL_PROJECT_TEST_APPDATA_PA
 set(TEST_PROJECT "AllTestProject")
 add_definitions("-DTEST_PROJECT=\"${TEST_PROJECT}\"")
 
-set(INCLUDE_DIR ${ALL_PROJECT_PATH}/source/include)
+set(INCLUDE_DIR ${ALL_PROJECT_PATH}/source/source)
 set(THIRD_PARTY_INCLUDE_DIR ${INCLUDE_DIR}/3party)
 
 include_directories(${INCLUDE_DIR})
@@ -39,7 +39,7 @@ include_directories(${THIRD_PARTY_INCLUDE_DIR}/json-develop/include)
 set(BACKEND_TEST_PROJECT "BackendTestProject")
 add_definitions("-DBACKEND_TEST_PROJECT=\"${BACKEND_TEST_PROJECT}\"")
 
-set(BACKEND_INCLUDE_DIR ${ALL_PROJECT_PATH}/source/backend/include)
+set(BACKEND_INCLUDE_DIR ${ALL_PROJECT_PATH}/source/backend/source)
 set(THIRD_PARTY_BACKEND_DIR ${BACKEND_INCLUDE_DIR}/3party)
 
 include_directories(${BACKEND_INCLUDE_DIR})
