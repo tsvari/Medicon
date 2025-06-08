54d4f0dff2c54bf2de61c07deb5595c29787b579
tsvari <tsvari@hotmail.com>
2025-06-08 10:13:12 -0400

    Extend JsonParameterFormatter class

--- source/source/JsonParameterFormatter.h
+++ source/source/JsonParameterFormatter.h
@@ -19,6 +19,9 @@ public:
     void AddDataInfo(const char * paramName, int64_t paramValue);
     void AddDataInfo(const char * paramName, double paramValue);
     void AddDataInfo(const char * paramName, bool paramValue);
+
+    void AddDataInfo(const char * paramName,  std::chrono::sys_seconds paramValue, DataInfo::Type nType);
+    void AddDataInfo(const char * paramName,  const char * paramValue, DataInfo::Type nType);
 };
 
 #endif // JSONPARAMETERFORMATTER_H
