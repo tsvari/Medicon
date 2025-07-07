1842f499f77357f7f1f9af65ff9781b44c0e422f
tsvari <tsvari@hotmail.com>
2025-07-07 16:35:16 -0400

    New functions to use in Tests

--- source/frontend/source/TestSharedUtility.cpp
+++ source/frontend/source/TestSharedUtility.cpp
@@ -1,2 +1,19 @@
 #include "TestSharedUtility.h"
 
+bool compareQVariant(const QVariant & lhs, const QVariant & rhs) {
+    return QVariant::compare(lhs, rhs) == QPartialOrdering::Equivalent;
+}
+
+bool compareQVariantList(const QList<QVariant> & lhs, const QList<QVariant> & rhs) {
+    if(lhs.size() != rhs.size()) {
+        return false;
+    }
+    QListIterator<QVariant> it1(lhs);
+    QListIterator<QVariant> it2(rhs);
+    while (it1.hasNext() && it2.hasNext()) {
+        if(!compareQVariant(it1.next(), it2.next())) {
+            return false;
+        }
+    }
+    return true;
+}
--- source/frontend/source/include_frontend_util.cpp
+++ source/frontend/source/include_frontend_util.cpp
@@ -12,4 +12,20 @@ std::string to_str(const QString & source) {
 std::string to_str(const QVariant & source) {
     return source.toString().toStdString();
 }
+
+QVariant to_qvariant_get(GrpcVariantGet varData) {
+    if (int32_t * ptr = std::get_if<int32_t>(&varData)) {
+        return QVariant::fromValue(*ptr);
+    } else if (int64_t * ptr = std::get_if<int64_t>(&varData)) {
+        return QVariant::fromValue(*ptr);
+    } else if (double * ptr = std::get_if<double>(&varData)) {
+        return QVariant::fromValue(*ptr);
+    } else if (auto ptr = std::get_if<std::reference_wrapper<const std::string>>(&varData)) {
+        return QString::fromStdString(ptr->get());
+    } else if (bool * ptr = std::get_if<bool>(&varData)) {
+        return QVariant::fromValue(*ptr);
+    } else {
+        return QVariant();
+    }
+}
 }
