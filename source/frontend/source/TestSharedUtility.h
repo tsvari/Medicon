851d7d2137cce37164f724f1217d967c6378f3d5
tsvari <tsvari@hotmail.com>
2025-07-07 16:35:16 -0400

    New functions to use in Tests

--- source/frontend/source/TestSharedUtility.h
+++ source/frontend/source/TestSharedUtility.h
@@ -21,4 +21,57 @@ auto pullout(const QItemSelection & sel, int role) {
     return pullout<TpT>(sel, func);
 }
 
+bool compareQVariant(const QVariant & lhs, const QVariant & rhs);
+
+bool compareQVariantList(const QList<QVariant> & lhs, const QList<QVariant> & rhs);
+
+template<typename T>
+const T& deref_if_refwrap(const T& val) {
+    return val;
+}
+
+template<typename T>
+const T& deref_if_refwrap(const std::reference_wrapper<T>& ref) {
+    return ref.get();
+}
+
+template<typename... Types>
+bool loose_compare(const std::variant<Types...>& a, const std::variant<Types...>& b) {
+    return std::visit([](const auto& lhs_raw, const auto& rhs_raw) -> bool {
+        const auto& lhs = deref_if_refwrap(lhs_raw);
+        const auto& rhs = deref_if_refwrap(rhs_raw);
+
+        using L = std::decay_t<decltype(lhs)>;
+        using R = std::decay_t<decltype(rhs)>;
+
+        if constexpr (std::is_same_v<L, R>) {
+            return lhs == rhs;
+        }
+        else if constexpr (std::is_arithmetic_v<L> && std::is_arithmetic_v<R>) {
+            //return lhs == rhs;
+            using Common = std::common_type_t<L, R>;
+            return static_cast<Common>(lhs) == static_cast<Common>(rhs);
+        }
+        else {
+            return false;
+        }
+    }, a, b);
+}
+
+// Compare two vectors of variants
+template<typename... Types>
+bool loose_vector_compare(const std::vector<std::variant<Types...>>& v1,
+                          const std::vector<std::variant<Types...>>& v2) {
+    if (v1.size() != v2.size())
+        return false;
+
+    for (size_t i = 0; i < v1.size(); ++i) {
+        if (!loose_compare(v1[i], v2[i]))
+            return false;
+    }
+
+    return true;
+}
+
+
 #endif // TESTSHAREDUTILITY_H
--- source/frontend/source/include_frontend_util.h
+++ source/frontend/source/include_frontend_util.h
@@ -1,8 +1,11 @@
-#pragma
+#pragma once
 
 #include "include_util.h"
 
 #include <QString>
+#include <QItemSelection>
+#include <QModelIndex>
+#include <QVariant>
 
 #ifdef Q_OS_MAC
 //#define PROJECT_PATH "/Users/tsvari/tida-web/"
@@ -10,11 +13,17 @@
 //#define PROJECT_PATH "/home/vaky/tida-web/"
 #endif
 
+using GrpcVariantGet = std::variant<int32_t, std::reference_wrapper<const std::string>, int64_t, bool, double>;
 namespace FrontConverter {
+
 QString to_str(const std::string & source);
 std::string to_str(const QString & source);
 std::string to_str(const QVariant & source);
+
+QVariant to_qvariant_get(GrpcVariantGet varData);
 }
+
+
 
 
 
