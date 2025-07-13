c99fceb1907833a9fc9aa85e8357d7ef3b1bd09a
tsvari <tsvari@hotmail.com>
2025-07-13 16:06:01 -0400

    New GrpcDataCOntainer and GrpcObjectTableModel classes with unit tests
    * New project for testing Frontend global classes

---
+++ source/frontend/source/tests/GrpcDataContainerTests.cpp
@@ -0,0 +1,248 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "TypeToStringFormatter.h"
+#include "TestSharedUtility.h"
+#include "GrpcDataContainer.hpp"
+
+#include <QDateTime>
+
+using ::testing::ElementsAre;
+using ::testing::Pointwise;
+
+namespace {
+void compareObjects (const GprcTestDataObject & left, const GprcTestDataObject & right) {
+    EXPECT_EQ(left.name(), right.name());
+    EXPECT_EQ(TimeFormatHelper::chronoSysSecToString(left.date(), DataInfo::Date), TimeFormatHelper::chronoSysSecToString(right.date(), DataInfo::Date));
+    EXPECT_EQ(left.height(), right.height());
+    EXPECT_EQ(left.salary(), right.salary());
+    EXPECT_EQ(left.married(), right.married());
+}
+
+// Company object has 5 properties
+const int PROPERTIES = 5;
+}
+
+TEST(GrpcDataContainerTests, GprcTestDataObjectTest)
+{
+    QDateTime current = QDateTime::currentDateTime();
+
+    GprcTestDataObject obj1;
+    obj1.set_name("Givi");
+    obj1.set_date(current.toSecsSinceEpoch());
+    obj1.set_height(168);
+    obj1.set_salary(12.15);
+    obj1.set_married(false);
+
+    GprcTestDataObject obj2;
+    obj2.set_name("Keto");
+    obj2.set_date(current.toSecsSinceEpoch());
+    obj2.set_height(164);
+    obj2.set_salary(30.557);
+    obj2.set_married(true);
+
+    std::vector<GprcTestDataObject> objects;
+    objects.push_back(obj1);
+    objects.push_back(obj2);
+
+    GrpcDataContainer<GprcTestDataObject> container(std::move(objects));
+
+    container.addProperty("Name", DataInfo::String, &GprcTestDataObject::set_name, &GprcTestDataObject::name);
+    container.addProperty("Date", DataInfo::Date, &GprcTestDataObject::set_date, &GprcTestDataObject::date);
+    container.addProperty("Height", DataInfo::Int, &GprcTestDataObject::set_height, &GprcTestDataObject::height);
+    container.addProperty("Salary", DataInfo::Double, &GprcTestDataObject::set_salary, &GprcTestDataObject::salary);
+    container.addProperty("Married", DataInfo::Bool, &GprcTestDataObject::set_married, &GprcTestDataObject::married);
+    container.initialize();
+
+    // vector shouls be empty after using
+    //EXPECT_TRUE(objects.empty());
+
+    GprcTestDataObject CompanyExpect1 = container.object(0);
+    GprcTestDataObject CompanyExpect2 = container.object(1);
+
+    // Check objects
+    compareObjects(obj1, CompanyExpect1);
+    compareObjects(obj2, CompanyExpect2);
+
+    // Check properies count
+    EXPECT_EQ(container.propertyCount(), PROPERTIES);
+
+    std::vector<GrpcVariantGet> obj1Var = {
+        obj1.name(),
+        obj1.date(),
+        obj1.height(),
+        obj1.salary(),
+        obj1.married()
+    };
+    std::vector<GrpcVariantGet> actual1 = {
+        container.nativeData(0, 0),
+        container.nativeData(0, 1),
+        container.nativeData(0, 2),
+        container.nativeData(0, 3),
+        container.nativeData(0, 4)
+    };
+    //=========================================
+    EXPECT_TRUE(loose_vector_compare(obj1Var, actual1));
+    ///////////////////////////////////////////////////////////
+    /// \brief
+    ///
+
+    std::vector<GrpcVariantGet> obj2Var = {
+       obj2.name(),
+       obj2.date(),
+       obj2.height(),
+       obj2.salary(),
+       obj2.married()
+    };
+    std::vector<GrpcVariantGet> actual2 = {
+        container.nativeData(1, 0),
+        container.nativeData(1, 1),
+        container.nativeData(1, 2),
+        container.nativeData(1, 3),
+        container.nativeData(1, 4)
+    };
+    //=========================================
+    EXPECT_TRUE(loose_vector_compare(obj2Var, actual2));
+    //////////////////////////////////////////////////////
+    /// \brief
+    ///
+
+    QList<QVariant> obj1QVar = {
+        FrontConverter::to_qvariant_get(obj1.name()),
+        FrontConverter::to_qvariant_get(obj1.date()),
+        FrontConverter::to_qvariant_get(obj1.height()),
+        FrontConverter::to_qvariant_get(obj1.salary()),
+        FrontConverter::to_qvariant_get(obj1.married()),
+    };
+    QList<QVariant> actualQVar1 = {
+        container.data(0, 0),
+        container.data(0, 1),
+        container.data(0, 2),
+        container.data(0, 3),
+        container.data(0, 4)
+    };
+    //=========================================
+    EXPECT_TRUE(compareQVariantList(obj1QVar, actualQVar1));
+    //////////////////////////////////////////////////////
+    /// \brief
+    ///
+
+    QList<QVariant> obj2QVar = {
+        FrontConverter::to_qvariant_get(obj2.name()),
+        FrontConverter::to_qvariant_get(obj2.date()),
+        FrontConverter::to_qvariant_get(obj2.height()),
+        FrontConverter::to_qvariant_get(obj2.salary()),
+        FrontConverter::to_qvariant_get(obj2.married())
+    };
+
+    QList<QVariant> actualQVar2 = {
+        container.data(1, 0),
+        container.data(1, 1),
+        container.data(1, 2),
+        container.data(1, 3),
+        container.data(1, 4)
+    };
+    EXPECT_TRUE(compareQVariantList(obj2QVar, actualQVar2));
+    //////////////////////////////////////////////////////
+    /// \brief
+    ///
+
+    std::string newName = "Vakho";
+    int64_t newDate = 12345;
+    int32_t newHeight = 180;
+    double newSalary = 56.78;
+    bool newMarried = true;
+
+    GprcTestDataObject actualObject;
+
+    QVariant newVariant = QString::fromStdString(newName);
+    GrpcVariantSet newGrpcVariant = newName;
+    container.setData(0,0, newGrpcVariant);
+    container.setData(1,0, newVariant);
+    actualObject.set_name(newName);
+
+    newVariant = newDate;
+    newGrpcVariant = newDate;
+    container.setData(0,1, newGrpcVariant);
+    container.setData(1,1, newVariant);
+    actualObject.set_date(newDate);
+
+    newVariant = newHeight;
+    newGrpcVariant = newHeight;
+    container.setData(0,2, newGrpcVariant);
+    container.setData(1,2, newVariant);
+    actualObject.set_height(newHeight);
+
+    newVariant = newSalary;
+    newGrpcVariant = newSalary;
+    container.setData(0,3, newGrpcVariant);
+    container.setData(1,3, newVariant);
+    actualObject.set_salary(newSalary);
+
+    newVariant = newMarried;
+    newGrpcVariant = newMarried;
+    container.setData(0,4, newGrpcVariant);
+    container.setData(1,4, newVariant);
+    actualObject.set_married(newMarried);
+
+    compareObjects(actualObject, container.object(0));
+    compareObjects(actualObject, container.object(1));
+
+    // second
+    container.insert(1, obj1);
+    EXPECT_TRUE(container.count() == 3);
+    // CHeck inserted object data
+    EXPECT_EQ(FrontConverter::to_str(container.data(1, 0)), obj1.name());
+    EXPECT_EQ(container.data(1, 1).toLongLong(), obj1.date());
+    EXPECT_EQ(container.data(1, 2).toInt(), obj1.height());
+    EXPECT_EQ(container.data(1, 3).toDouble(), obj1.salary());
+    EXPECT_EQ(container.data(1, 4).toBool(), obj1.married());
+
+    EXPECT_EQ("Vakho", container.data(0, 0).toString());
+    EXPECT_EQ("Givi", container.data(1, 0).toString());
+    EXPECT_EQ("Vakho", container.data(2, 0).toString());
+
+    // last/forth
+    container.addNew(obj2);
+    EXPECT_TRUE(container.count() == 4);
+    // CHeck added object data
+    EXPECT_EQ(FrontConverter::to_str(container.data(3, 0)), obj2.name());
+    EXPECT_EQ(container.data(3, 1).toLongLong(), obj2.date());
+    EXPECT_EQ(container.data(3, 2).toInt(), obj2.height());
+    EXPECT_EQ(container.data(3, 3).toDouble(), obj2.salary());
+    EXPECT_EQ(container.data(3, 4).toBool(), obj2.married());
+
+    EXPECT_EQ("Vakho", container.data(0, 0).toString());
+    EXPECT_EQ("Givi", container.data(1, 0).toString());
+    EXPECT_EQ("Vakho", container.data(2, 0).toString());
+    EXPECT_EQ("Keto", container.data(3, 0).toString());
+
+    compareObjects(obj1, container.object(1));
+    compareObjects(obj2, container.object(3));
+
+    container.remove(2);
+    EXPECT_EQ(container.count(), 3);
+
+    container.remove(0);
+    EXPECT_EQ(container.count(), 2);
+
+    compareObjects(obj1, container.object(0));
+    compareObjects(obj2, container.object(1));
+
+    EXPECT_EQ("Givi", container.data(0, 0).toString());
+    EXPECT_EQ("Keto", container.data(1, 0).toString());
+
+    GprcTestDataObject newObj;
+    newObj.set_name("Vakho");
+    newObj.set_date(current.toSecsSinceEpoch());
+    newObj.set_height(175);
+    newObj.set_salary(145.123);
+    newObj.set_married(true);
+
+    container.update(1, newObj);
+    compareObjects(newObj, container.object(1));
+    EXPECT_EQ("Givi", container.data(0, 0).toString());
+    EXPECT_EQ("Vakho", container.data(1, 0).toString());
+}
+
+
---
+++ source/frontend/source/tests/GrpcObjectTableModelTests.cpp
@@ -0,0 +1,153 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "GrpcObjectTableModel.h"
+#include "TestSharedUtility.h"
+
+#include <QDateTime>
+
+
+using ::testing::ElementsAre;
+
+namespace {
+const int columnCount = 5;
+}
+
+TEST(GrpcObjectTableModelTests, GprcBasicTest)
+{
+    QDateTime current = QDateTime::currentDateTime();
+
+    GprcTestDataObject obj1;
+
+    obj1.set_name("Givi");
+    obj1.set_date(current.toSecsSinceEpoch());
+    obj1.set_height(168);
+    obj1.set_salary(12.15);
+    obj1.set_married(false);
+
+    GprcTestDataObject obj2;
+
+    obj2.set_name("Keto");
+    obj2.set_date(current.toSecsSinceEpoch());
+    obj2.set_height(164);
+    obj2.set_salary(30.557);
+    obj2.set_married(true);
+
+    std::vector<GprcTestDataObject> objects;
+    objects.push_back(obj1);
+    objects.push_back(obj2);
+
+    GrpcTestObjectTableModel model(std::move(objects));
+    EXPECT_EQ(model.rowCount(), 2);
+    EXPECT_EQ(model.columnCount(), columnCount);
+
+    // First row
+    auto actualRow1 = pullout<QString>(
+        {model.index(0,0), model.index(0, model.columnCount() - 1)},
+        Qt::DisplayRole
+        );
+    EXPECT_THAT(actualRow1, ElementsAre("Givi",
+                                    std::to_string(current.toSecsSinceEpoch()),
+                                    "168",
+                                    "12.15",
+                                    "false"));
+
+    // Second row
+    auto actualRow2 = pullout<QString>(
+        {model.index(1,0), model.index(1, columnCount - 1)},
+        Qt::DisplayRole
+        );
+
+    EXPECT_THAT(actualRow2, ElementsAre("Keto",
+                                        std::to_string(current.toSecsSinceEpoch()),
+                                        "164",
+                                        "30.557",
+                                        "true"));
+
+    GprcTestDataObject obj3;
+    obj3.set_name("Vakho");
+    obj3.set_date(current.toSecsSinceEpoch());
+    obj3.set_height(175);
+    obj3.set_salary(135000.567);
+    obj3.set_married(true);
+
+    // Second Row again
+    model.insertObject(1, QVariant::fromValue<GprcTestDataObject>(obj3));
+    EXPECT_EQ(model.rowCount(), 3);
+EXPECT_EQ("Givi", model.index(0,0).data().toString());
+    auto actualRowInserted = pullout<QString>(
+        {model.index(1,0), model.index(1, columnCount - 1)},
+        Qt::DisplayRole
+        );
+    EXPECT_THAT(actualRowInserted, ElementsAre("Vakho",
+                                        std::to_string(current.toSecsSinceEpoch()),
+                                        "175",
+                                        "135000.567",
+                                        "true"));
+
+    GprcTestDataObject obj4;
+    obj4.set_name("Elene");
+    obj4.set_date(current.toSecsSinceEpoch());
+    obj4.set_height(155);
+    obj4.set_salary(567);
+    obj4.set_married(false);
+
+    // Forth row
+    model.addNewObject(QVariant::fromValue<GprcTestDataObject>(obj4));
+    EXPECT_EQ(model.rowCount(), 4);
+
+    auto actualRowAdded = pullout<QString>(
+        {model.index(3,0), model.index(3, columnCount - 1)},
+        Qt::DisplayRole
+        );
+    EXPECT_THAT(actualRowAdded, ElementsAre("Elene",
+                                               std::to_string(current.toSecsSinceEpoch()),
+                                               "155",
+                                               "567",
+                                               "false"));
+    // Check names only first column
+    auto actualNamesFirstColumn = pullout<QString>(
+        {model.index(0,0), model.index(3, 0)},
+        Qt::DisplayRole
+        );
+    EXPECT_THAT(actualNamesFirstColumn, ElementsAre("Givi",
+                                            "Vakho",
+                                            "Keto",
+                                            "Elene"));
+    // Update object test
+    GprcTestDataObject newObj;
+    newObj.set_name("Teona");
+    newObj.set_date(current.toSecsSinceEpoch());
+    newObj.set_height(166);
+    newObj.set_salary(5.123);
+    newObj.set_married(true);
+
+    model.updateObject(3, QVariant::fromValue<GprcTestDataObject>(newObj));
+    auto actualRowUpdated = pullout<QString>(
+        {model.index(3,0), model.index(3, columnCount - 1)},
+        Qt::DisplayRole
+        );
+    EXPECT_THAT(actualRowUpdated, ElementsAre("Teona",
+                                            std::to_string(current.toSecsSinceEpoch()),
+                                            "166",
+                                            "5.123",
+                                            "true"));
+
+    // Header tests
+    auto actualHeadersHorizontal = pulloutHeader<QString>(&model, {0, 1, 2, 3, 4}, Qt::Horizontal, Qt::DisplayRole);
+    EXPECT_THAT(actualHeadersHorizontal, ElementsAre(
+                                        "Name",
+                                        "Date",
+                                        "Height",
+                                        "Salary",
+                                        "Married"));
+
+    auto actualHeadersVertical = pulloutHeader<QString>(&model, {0, 1, 2, 3}, Qt::Vertical, Qt::DisplayRole);
+    EXPECT_THAT(actualHeadersVertical, ElementsAre(
+                                             "1",
+                                             "2",
+                                             "3",
+                                             "4"));
+
+
+}
