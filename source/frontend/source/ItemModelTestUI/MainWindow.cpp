d7f016d2dc1437f88ef54eaa48ed6b81b0b56a03
tsvari <tsvari@hotmail.com>
2025-09-26 16:26:02 -0400

    Split data source and test utility

--- source/frontend/source/ItemModelTestUI/MainWindow.cpp
+++ source/frontend/source/ItemModelTestUI/MainWindow.cpp
@@ -1,6 +1,6 @@
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
-#include "TestSharedUtility.h"
+#include "TestDataSource.h"
 #include "GrpcProxySortFilterModel.h"
 #include "GrpcTemplateController.h"
 #include "GrpcMasterSlaveController.h"
