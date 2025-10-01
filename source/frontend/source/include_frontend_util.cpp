2c86d5bb08dd297cc27b76f0a1c32f3ced1a3efa
tsvari <tsvari@hotmail.com>
2025-10-01 13:03:19 -0400

    Add formatting function to display Date-Time types of data in model

--- source/frontend/source/include_frontend_util.cpp
+++ source/frontend/source/include_frontend_util.cpp
@@ -1,6 +1,8 @@
 #include "include_frontend_util.h"
 
 #include <QVariant>
+#include <chrono>
+#include <format>
 
 namespace FrontConverter {
 QString to_str(const std::string & source) {
@@ -28,4 +30,42 @@ QVariant to_qvariant_get(GrpcVariantGet varData) {
         return QVariant();
     }
 }
+
+QVariant to_qvariant_by_type(const GrpcVariantGet & varData, DataInfo::Type type) {
+    const QVariant & qVariantData = to_qvariant_get(varData);
+    std::string retData;
+    switch(type) {
+    case DataInfo::Double:
+        // temporary
+        return qVariantData;
+    case DataInfo::DateTime:
+    case DataInfo::DateTimeNoSec:
+    case DataInfo::Date:
+    case DataInfo::Time: {
+        using namespace std::chrono;
+        milliseconds ms_since_epoch{qVariantData.toLongLong()};
+        sys_seconds tp_s = time_point_cast<seconds>(sys_time<milliseconds>{ms_since_epoch});
+        std::chrono::zoned_time local{std::chrono::current_zone(), tp_s};
+        std::locale loc(""); // system locale
+        if(type == DataInfo::Date) {
+            retData = std::format(loc, "{:L%x}", local);
+        } else if(type == DataInfo::Time) {
+            retData = std::format(loc, "{:L%X}", local);
+        } else if(type == DataInfo::DateTime) {
+            retData = std::format(loc, "{:L%c}", local);
+        } else if(type == DataInfo::DateTimeNoSec) {
+            retData = std::format(loc, "{:L%c}", local); // full locale date+time
+            // Find last colon (start of seconds field in time portion)
+            auto pos = retData.rfind(':');
+            if (pos != std::string::npos && pos + 3 <= retData.size()) {
+                retData.erase(pos, 3);  // erase ":ss"
+            }
+        }
+        return QString::fromStdString(retData);
+    }
+    default:
+        return qVariantData;
+    }
+}
+
 }
