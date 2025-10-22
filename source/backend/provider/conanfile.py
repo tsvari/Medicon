640800b22dbd9b7a2f6b70c1f2ecb9c916c2b95f
tsvari <tsvari@hotmail.com>
2025-10-22 13:11:21 -0400

    Fix conan file issue in provider
    * sentry-native was installed and built with conan

--- source/backend/provider/conanfile.py
+++ source/backend/provider/conanfile.py
@@ -1,31 +1,24 @@
 import os
 from conan import ConanFile
 from conan.tools.files import get, copy, download
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.scm import Version
-from conan.tools.cmake import CMake, CMakeDeps, cmake_layout
+from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
 
 class provider(ConanFile):
     name = "provider"
     version = "0.1.0"
-    
+
     url = "url.com"
     description = ""
     settings = "os", "compiler", "build_type", "arch"
     #exports_sources = "CMakeLists.txt", "../include/CrashDump.h", "../source/CrashDump.cpp"
     #default_options = "toolchain:cmake=True"
- 
-    tool_requires = (
-    "easyloggingpp/9.97.1",
-    #"sentry-native/0.8.1",
-    "gtest/1.16.0"
-    )
 
-    generators = "CMakeToolchain"
+    requires = "easyloggingpp/9.97.1", "gtest/1.16.0", "sentry-native/0.11.3"
 
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
 
-    def layout(self):
-       cmake_layout(self)
+        deps = CMakeDeps(self)
+        deps.generate()
+
